73c75414d570d49ca64b266769dfc58bf82ce761
kki2020 <kki2020@daum.net>
2022-10-14 18:02:06 +0900

    2

--- knusSandbox/Shared/ApiHelper.cs
+++ knusSandbox/Shared/ApiHelper.cs
@@ -1,6 +1,87 @@
-﻿namespace knusSandbox.Shared
+﻿using knusSand.Infrastructure.ApiClient;
+using knusSandbox.Components.Common;
+using MudBlazor;
+
+namespace knusSandbox.Shared
 {
-    public class ApiHelper
+    public static class ApiHelper
     {
+        public static async Task<T?> ExecuteCallGuardedAsync<T>(
+            Func<Task<T>> call,
+            ISnackbar snackbar,
+            CustomValidation? customValidation = null,
+            string? successMessage = null)
+        {
+            customValidation?.ClearErrors();
+            try
+            {
+                var result = await call();
+
+                if (!string.IsNullOrWhiteSpace(successMessage))
+                {
+                    snackbar.Add(successMessage, Severity.Info);
+                }
+
+                return result;
+            }
+            catch (ApiException<HttpValidationProblemDetails> ex)
+            {
+                if (ex.Result.Errors is not null)
+                {
+                    customValidation?.DisplayErrors(ex.Result.Errors);
+                }
+                else
+                {
+                    snackbar.Add("Something went wrong!", Severity.Error);
+                }
+            }
+            catch (ApiException<ErrorResult> ex)
+            {
+                snackbar.Add(ex.Result.Exception, Severity.Error);
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, Severity.Error);
+            }
+
+            return default;
+        }
+
+        public static async Task<bool> ExecuteCallGuardedAsync(
+            Func<Task> call,
+            ISnackbar snackbar,
+            CustomValidation? customValidation = null,
+            string? successMessage = null)
+        {
+            customValidation?.ClearErrors();
+            try
+            {
+                await call();
+
+                if (!string.IsNullOrWhiteSpace(successMessage))
+                {
+                    snackbar.Add(successMessage, Severity.Success);
+                }
+
+                return true;
+            }
+            catch (ApiException<HttpValidationProblemDetails> ex)
+            {
+                if (ex.Result.Errors is not null)
+                {
+                    customValidation?.DisplayErrors(ex.Result.Errors);
+                }
+                else
+                {
+                    snackbar.Add("Something went wrong!", Severity.Error);
+                }
+            }
+            catch (ApiException<ErrorResult> ex)
+            {
+                snackbar.Add(ex.Result.Exception, Severity.Error);
+            }
+
+            return false;
+        }
     }
 }
